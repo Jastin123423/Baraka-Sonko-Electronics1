e3e35fa8215b270007f2ee369a43a0fa8d3b7b44
Jastin123423 <Jjovinatha@gmail.com>
2026-02-02 15:25:21 +0300

    Update AdminView.tsx

--- components/AdminView.tsx
+++ components/AdminView.tsx
@@ -1,3 +1,4 @@
+//AdminView.tsx 
 import React, { useState, useEffect } from 'react';
 import { Product, Order, AdminStats, Category } from '../types';
 // REMOVED: import { CATEGORIES } from '../constants'; // Use dynamic categories from props
