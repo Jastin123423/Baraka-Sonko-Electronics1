8321d143a92359c997865d8e49368895c0914ee3
Jastin123423 <Jjovinatha@gmail.com>
2026-02-03 18:39:20 +0300

    Update AdminView.tsx

--- components/AdminView.tsx
+++ components/AdminView.tsx
@@ -1,4 +1,4 @@
-//AdminView.tsx 
+
 
 import React, { useState, useEffect } from 'react';
 import { Product, Category, AdminStats } from '../types';
@@ -1162,4 +1162,4 @@ const AdminView: React.FC<AdminViewProps> = ({
   );
 };
 
-export { AdminView };
+export default AdminView;
